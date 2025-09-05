ae5033e84597b71a6dc3f6789ba49764cd97a069
pipi-shortstocking <jannie435@naver.com>
2025-09-05 21:11:45 +0900

    feat: 이메일 중복 커스텀 및 닉네임 자동생성(#7)

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 from users.models import User
 
 
 class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        validators=[UniqueValidator(queryset=User.objects.all(), message="이미 사용 중인 이메일입니다.")]
+    )
     password2 = serializers.CharField(write_only=True)
 
     class Meta:
@@ -13,26 +17,30 @@ class RegisterSerializer(serializers.ModelSerializer):
             'password': {'write_only': True}
         }
 
+    # def validate_email(self, data):
+    #     if User.objects.filter(email=data).exists():
+    #         raise serializers.ValidationError("이미 사용 중인 이메일입니다.")
+    #
+    #     return data
+
+    def validate_password(self, data):
+        # 비밀번호 규칙 검사(8~20자, 영문/숫자/특수문자 포함)
+        import re
+        if len(data) < 8 or len(data) > 20:
+            raise serializers.ValidationError("비밀번호는 8~20자여야 합니다.")
+        if not re.search(r'[A-Za-z]', data) or not re.search(r'[0-9]', data) or not re.search(r'[^A-Za-z0-9]', data):
+            raise serializers.ValidationError("비밀번호는 영문,숫자,특수문자를 모두 포함해야 합니다.")
+
+        return data
+
     def validate(self, data):
         # 비밀번호 확인
         if data['password'] != data['password2']:
             raise serializers.ValidationError("비밀번호가 일치하지 않습니다.")
 
-        # 비밀번호 규칙 검사(8~20자, 영문/숫자/특수문자 포함)
-        import re
-        password = data['password']
-        if len(password) < 8 or len(password) > 20:
-            raise serializers.ValidationError("비밀번호는 8~20자여야 합니다.")
-        if not re.search(r'[A-Za-z]', password) or not re.search(r'[0-9]', password) or not re.search(r'[^A-Za-z0-9]', password):
-            raise serializers.ValidationError("비밀번호는 영문,숫자,특수문자를 모두 포함해야 합니다.")
-
         return data
 
     def create(self, validated_data):
         validated_data.pop('password2') # DB에는 password2 저장 X
-        user = User.objects.create_user(
-            email=validated_data['email'],
-            password=validated_data['password'],
-            nickname=validated_data.get('nickname',""),
-        )
-        return user
+
+        return User.objects.create_user(**validated_data)
