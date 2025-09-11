b1b232c585c165865ff7ad7daf33a783147845e5
pipi-shortstocking <jannie435@naver.com>
2025-09-11 17:00:37 +0900

    feat: products 테이블 생성 중(#14)

---
+++ products/models.py
@@ -0,0 +1,11 @@
+# from django.db import models
+#
+# class Product(models.Model):
+#     product = models.TextField(verbose_name="제품 이름")
+#     company = models.TextField(verbose_name="회사 이름")
+#     price = models.IntegerField(verbose_name="가격")
+#     # gram = models.IntegerField(verbose_name="그램")
+#     unit = models.TextField(verbose_name="단위")
+#     piece = models.IntegerField(verbose_name="개수")
+#     nutrients = models.TextField(verbose_name="영양정보")
+#     viewCount = models.IntegerField
