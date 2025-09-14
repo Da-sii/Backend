7d7c32a49ce55f8cae617b16f55134bc3fe88691
pipi-shortstocking <jannie435@naver.com>
2025-09-14 22:25:20 +0900

    feat: 메인 화면(#32)

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from products.views import ProductCreateView, ProductDetailView, ProductRankingView, ProductListView, ProductCategoryView, ProductSearchView
+from products.views import ProductCreateView, ProductDetailView, ProductRankingView, ProductListView, ProductCategoryView, ProductSearchView, MainView
 
 urlpatterns = [
     path("add/", ProductCreateView.as_view(), name="product_add"),
@@ -8,7 +8,8 @@ urlpatterns = [
     path("ranking/", ProductRankingView.as_view(), name="product_ranking"),
     path("list/", ProductListView.as_view(), name="product_list"),
     path("category/", ProductCategoryView.as_view(), name="product_category"),
-    path("search/", ProductSearchView.as_view(), name="product_search")
+    path("search/", ProductSearchView.as_view(), name="product_search"),
+    path("main/", MainView.as_view(), name="product_main")
 ]
 
 
