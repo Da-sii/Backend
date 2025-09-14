2b4f31f667ddf6b9a5c06dc1c5a68917ca1517bd
pipi-shortstocking <jannie435@naver.com>
2025-09-14 20:53:26 +0900

    feat: 제품 리스트(#30)

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from products.views import ProductCreateView, ProductDetailView, ProductRankingView
+from products.views import ProductCreateView, ProductDetailView, ProductRankingView, ProductListView
 
 urlpatterns = [
     path("add/", ProductCreateView.as_view(), name="product_add"),
     path("<int:id>/", ProductDetailView.as_view(), name="product_detail"),
     path("ranking/", ProductRankingView.as_view(), name="product_ranking"),
+    path("list/", ProductListView.as_view(), name="product_list"),
 ]
 
 
