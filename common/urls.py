c5158834f348391f37aee9415fa091ce69e6f19f
pipi-shortstocking <jannie435@naver.com>
2026-04-30 22:35:24 +0900

    feat: 딥링크 설정

--- common/urls.py
+++ common/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import BannerListView
+from .views import BannerListView, apple_app_site_association, assetlinks
 
 urlpatterns = [
     path('', BannerListView.as_view()),
+    path('.well-known/apple-app-site-association', apple_app_site_association),
+    path('.well-known/assetlinks.json', assetlinks),
 ]
