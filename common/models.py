e7c493eab277b853bf3b9076eeb87d0205fb9ecc
pipi-shortstocking <jannie435@naver.com>
2026-05-04 23:15:51 +0900

    feat: 배너 상세 이미지 추가

--- common/models.py
+++ common/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Banner(models.Model):
     image_url = models.URLField()
+    detail_image_url = models.URLField(blank=True, null=True)
     order = models.PositiveIntegerField(default=0)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
