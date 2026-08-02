e438b8ba75b43d025e7af334e4b7a3f2b5292bd5
pipi-shortstocking <jannie435@naver.com>
2026-08-02 21:44:27 +0900

    fix: ALLOWED_HOSTS의 www.dasii.kr 마크다운 링크 형태 오류 수정

--- dasii_backend/settings.py
+++ dasii_backend/settings.py
@@ -262,7 +262,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
